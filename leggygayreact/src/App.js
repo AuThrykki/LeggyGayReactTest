f4b9e6538e4cde4669c6e11a5e901cd1c3385fca
Tóni <35340072+AuThrykki@users.noreply.github.com>
2024-03-18 15:08:01 +0100

    Master test

---
+++ leggygayreact/src/App.js
@@ -0,0 +1,17 @@
+import logo from './@Lay0wn_HevrosSticker.PNG';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Leggy test app.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
